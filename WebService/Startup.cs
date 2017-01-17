524bf0efd3b22c89732365ec1fedaababc159e63
Sean Feldman <feldman.sean@gmail.com>
2017-01-16 21:03:43 -0700

    Initial version with OwinCommunicationListener and Values controller

---
+++ WebService/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace WebService
+{
+    using System.Web.Http;
+    using Owin;
+
+    public static class Startup
+    {
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
